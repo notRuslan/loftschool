5555c0d673ff8c11b1237fc7243c730011d82a85
Ruslan <ruslangr@yahoo.com>
2017-07-30 14:34:47 +0300

    Start DZ2

---
+++ dz2/functions.php
@@ -0,0 +1,75 @@
+<?php
+
+function task1($str_arr = [], $ret_line = false)
+{
+    if (!$ret_line) {
+        $str = '';
+        foreach ($str_arr as $line) {
+            $str .= '<p>' . $line . PHP_EOL;
+        }
+        return $str;
+    }
+    return implode(' ', $str_arr);
+
+}
+
+function task2($arr_nums = [], $act = null)
+{
+    if(empty($arr_nums)){
+        return;
+    }
+
+    function add($arr = []){
+        #res =0;
+        foreach()
+    }
+    function diff(){
+
+    }
+    function mult(){
+
+    }
+    function div(){
+
+    }
+}
+
+function task3()
+{
+
+}
+
+function task4()
+{
+
+}
+
+function task5()
+{
+
+}
+
+function task6()
+{
+
+}
+
+function task7()
+{
+
+}
+
+function task8()
+{
+
+}
+
+function task9()
+{
+
+}
+
+function task10()
+{
+
+}
---
+++ dz2/index.php
@@ -0,0 +1,21 @@
+<?php
+# DZ 2
+require('functions.php');
+
+
+/*echo "Task1 :" . PHP_EOL;
+
+$arr_str =[
+    'HEllo world!',
+    'I have a cat',
+    'Nice to see you.'
+];
+
+echo task1($arr_str);
+echo task1($arr_str, true);*/
+
+$var = "10 + 2";
+
+echo eval("return $var;");
+
+
